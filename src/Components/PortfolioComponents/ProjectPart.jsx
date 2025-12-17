01331fba3ba3d6c3284314bf693ed28967ab7685
Vivek Sharma <viveksharmaa252@gmail.com>
2025-12-17 12:42:44 +0530

    formatting folder structure

---
+++ src/Components/PortfolioComponents/ProjectPart.jsx
@@ -0,0 +1,106 @@
+import PortfolioProjectCard from "../PortfolioProjectCard";
+import { motion,AnimatePresence } from "framer-motion";
+import {
+  h2Style,
+  primaryBtn,
+  projectBtnActive,
+  projectBtnBase,
+  serviceCardStyle,
+} from '../../Style/ComponentsStyle';
+import ProjectsCarousel from "../ProjectsCarousel";
+
+const ProjectPart = ({
+  filteredProjects,
+  isInView,
+  btnTitles,
+  isActive,
+  setIsActive,
+  setIsViewerOpen,
+  setCurrentIndex,
+  activeProject,
+  currentIndex,
+  isViewerOpen,
+  openViewer,
+}) => {
+  return (
+    <>
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ y: "100%", opacity: 0 }}
+          animate={isInView ? { y: 0, opacity: 1 } : { y: "100%", opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className="text-center"
+        >
+          <h2 className={h2Style}>Portfolio</h2>
+          <p className="text-gray-400 lg:text-lg">
+            Necessitatibus eius consequatur ex aliquid fuga eum quidem sint
+            consectetur velit
+          </p>
+        </motion.div>
+        <div className="my-5 lg:my-10">
+          <div className="flex gap-5 lg:gap-10 justify-center flex-wrap">
+            {btnTitles.map((title, index) => (
+              <motion.button
+                initial={{ scale: 0, opacity: 0 }}
+                animate={
+                  isInView ? { scale: 1, opacity: 1 } : { scale: 0, opacity: 0 }
+                }
+                transition={{ duration: 0.5 }}
+                key={index}
+                className={
+                  isActive === title ? projectBtnActive : projectBtnBase
+                }
+                onClick={() => setIsActive(title)}
+              >
+                {title}
+              </motion.button>
+            ))}
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 gap-y-5">
+          {filteredProjects.map((project, idx) => (
+            <PortfolioProjectCard
+              key={project.id}
+              {...project}
+              onView={() => openViewer(filteredProjects, idx)}
+            />
+          ))}
+        </div>
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ amount: 0.3, once: true }}
+          transition={{ duration: 0.5 }}
+          className={`mt-5 lg:mt-10 ${serviceCardStyle}`}
+        >
+          <h3 className="capitalize text-xl lg:text-2xl font-bold font-serif">
+            Ready to start your next project?
+          </h3>
+          <p className="text-gray-400 lg:text-lg">
+            Let's work together to bring your digital vision to life
+          </p>
+          <div>
+            <button className={`${primaryBtn} rounded-xl! mr-5 mb-5`}>
+              Start a Project
+            </button>
+            <button className={`${primaryBtn} rounded-xl! mr-5`}>
+              View All Work
+            </button>
+          </div>
+        </motion.div>
+      </div>
+      <AnimatePresence>
+        {isViewerOpen && (
+          <ProjectsCarousel
+            setIsViewerOpen={setIsViewerOpen}
+            setCurrentIndex={setCurrentIndex}
+            activeProject={activeProject}
+            currentIndex={currentIndex}
+          />
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default ProjectPart;
---
+++ src/Components/PortfolioComponents/WhyChooseUsPart.jsx
@@ -0,0 +1,59 @@
+import { Award, ChartLine, Palette } from "lucide-react";
+import ChooseCard from "../ChooseCard";
+import { h2Style, sectionBaseStyle } from "../../Style/ComponentsStyle";
+
+const WhyChooseUsPart = () => {
+  const cardData = [
+    {
+      id: 1,
+      title: "Creative Excellence",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam.",
+      number: "95",
+      bottomText: "% Client Satisfaction",
+      icon : <Palette size={40} />,
+    },
+    {
+      id: 2,
+      title: "Proven  Result",
+      desc: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit.",
+      number: "200",
+      bottomText: "% ROI Increase",
+      icon : <ChartLine size={40} />,
+    },
+    {
+      id: 3,
+      title: "Export Team",
+      desc: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum sed ut perspiciatis unde omnis.",
+      number: "50",
+      bottomText: "+ Awards won",
+      icon : <Award size={40} fill="currentColor" />,
+    },
+  ];
+  return (
+    <section className={sectionBaseStyle}>
+      <div className="container mx-auto">
+        <div className="text-center">
+          <h2 className={h2Style}>Why Us</h2>
+          <p className="text-gray-500 text-lg">
+            Necessitatibus eius consequatur ex aliquid fuga eum quidem sint
+            consectetur velit
+          </p>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-5 mt-5 lg:mt-10">
+          {cardData.map((card) => (
+            <ChooseCard
+              key={card.id}
+              title={card.title}
+              desc={card.desc}
+              number={card.number}
+              bottomText={card.bottomText}
+              icon = {card.icon}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhyChooseUsPart;

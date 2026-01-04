8d452bf7663715065e51f8d6c8e718c0c75aacae
Vivek Sharma <viveksharmaa252@gmail.com>
2026-01-04 14:35:11 +0530

    Fixing errors and bugs from diffrent part of project and added new components & sections

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div>
     <Header />
-    <main className=" text-white ">
+    <main className=" text-white forced-color-adjust-none">
       <HeroSection />
       <AboutSection />
       <ServicesSection />
